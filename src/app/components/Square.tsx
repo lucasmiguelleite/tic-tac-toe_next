edf45e7866bad83e6113f600005589b4a9577090
Lucas <lucasleite.miguel10@gmail.com>
2024-11-24 10:07:30 -0300

    style: board size change

--- src/app/components/Square.tsx
+++ src/app/components/Square.tsx
@@ -15,12 +15,12 @@ const Square = ({
 }) => {
   if (!value) {
     return (
-      <button className="w-52 h-52 text-7xl text-white border border-solid border-black rounded-lg hover:text-slate-300" onClick={onClick} disabled={Boolean(winner)}>{currentPlayer}</button>
+      <button className="max-w-60 max-h-60 min-h-24 min-w-24 text-7xl text-white border border-solid border-black rounded-lg hover:text-slate-300" onClick={onClick} disabled={Boolean(winner)}>{currentPlayer}</button>
     );
   }
   return (
     <button
-      className={`${value === 'X' ? "bg-green-500" : "bg-red-600"} w-52 h-52 text-7xl text-slate-200 border border-solid border-black rounded-lg`}
+      className={`${value === 'X' ? "bg-green-500" : "bg-red-600"} max-w-60 max-h-60 min-h-24 min-w-24 text-7xl text-slate-200 border border-solid border-black rounded-lg`}
       disabled>{value}</button>
   );
 }
