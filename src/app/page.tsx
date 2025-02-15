0570aa71b9b951d59cac8f9783ff26d8566e34f7
Lucas <lucasleite.miguel10@gmail.com>
2025-02-15 12:30:23 -0300

    chore: change the multiplayer button name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
             <button onClick={() => router.push('/single-player')} className="border rounded-full mr-2 text-center w-52 h-12 hover:bg-gray-600 hover:text-white "><p className="font-bold">Single Player</p></button>
           </div>
           <div className="flex justify-center mb-5">
-            <button onClick={() => router.push('/two-players-local')} className="border rounded-full mr-2 text-center w-52 h-12 hover:bg-gray-600 hover:text-white "><p className="font-bold">2 Players Local</p></button>
+            <button onClick={() => router.push('/two-players-local')} className="border rounded-full mr-2 text-center w-52 h-12 hover:bg-gray-600 hover:text-white "><p className="font-bold">Multiplayer Local</p></button>
           </div>
         </div>
       </div>
