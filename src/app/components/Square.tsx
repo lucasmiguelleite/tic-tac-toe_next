4d76d80fe0ef9c7b47350b46163b3a4a0ef779bb
Lucas <lucasleite.miguel10@gmail.com>
2025-02-18 14:03:48 -0300

    refactor: change the way are redirected on the 404 page

--- src/app/components/Square.tsx
+++ src/app/components/Square.tsx
@@ -15,7 +15,7 @@ const Square = ({
 }) => {
   if (!value) {
     return (
-      <button className="max-w-60 max-h-60 min-h-24 min-w-24 text-7xl text-white border border-solid border-black rounded-lg hover:text-slate-300" onClick={onClick} disabled={Boolean(winner)}>{currentPlayer}</button>
+      <button className="max-w-60 max-h-60 min-h-24 min-w-24 text-7xl text-background border border-solid border-black rounded-lg hover:text-slate-300" onClick={onClick} disabled={Boolean(winner)}>{currentPlayer}</button>
     );
   }
   return (
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,10 +1,13 @@
-import Link from 'next/link'
+'use client'
+import { redirect } from "next/navigation"
 
 export default function NotFound() {
-  return <div>
+  return (
+    <div>
       <h1 className='font-bold text-4xl my-10 text-center'>Page Not Found!</h1>
       <div className='flex flex-nowrap justify-center'>
-        <Link href="/"><button className='border rounded-full mr-2 mb-2 text-center w-52 h-16 hover:bg-gray-600 hover:text-white'>Go back to Home</button></Link>
+        <button className='border rounded-full mr-2 mb-2 text-center w-52 h-16 hover:bg-gray-600 hover:text-white' onClick={() => redirect("/")}>Go back to Home</button>
       </div>
   </div>
+  );
 }
