f77fbf92fc5d696319dd8bbb1f7d132c28b1fa74
Lucas Miguel Leite <lucasleite.miguel10@gmail.com>
2026-05-16 12:36:12 -0300

    style(home): update layout and remove link from heading

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,13 +1,11 @@
 const Home = () => {
   return (
-    <div>
-      <div className="my-10 md:mx-10 mx-2">
-      <a href="/">
-        <h1 className="font-bold text-6xl text-center md:text-left">Tic-Tac-Toe #</h1>
-      </a>
-      </div>
+    <div className="my-10 md:mx-10 mx-2 cursor-default">
+      <h1 className="font-bold text-6xl text-center md:text-left">
+        Tic-Tac-Toe #
+      </h1>
     </div>
-  )
-}
+  );
+};
 
 export default Home;
